0296cf599647fcc7cd3c4becdcc12fe77d40751a
Juanfe-engineer <juanf.ibarral@uqvirtual.edu.co>
2026-05-23 23:00:08 -0500

    Sistema de sobres - Open pack

--- lib/pokemon_battle/sistema_sobres.ex
+++ lib/pokemon_battle/sistema_sobres.ex
@@ -28,5 +28,40 @@ defmodule PokemonBattle.SistemaSobres do
     end
   end
 
-  
+  # Open pack
+
+  def open_pack(trainer, pack_id) do
+    packs = trainer["packs"]
+
+    pack = case pack_id do
+      "last" -> List.last(packs)
+      _      -> Enum.find(packs, fn p -> to_string(p["id"]) == to_string(pack_id) end)
+    end
+
+    case pack do
+      nil ->
+        {:error, "Pack not found"}
+
+      found_pack ->
+        shop     = Persistencia.read_shop()
+        pack_cfg = shop[found_pack["type"]]
+        probs    = pack_cfg["probabilities"]
+
+        new_pokemon = Enum.map(1..3, fn _ ->
+          species = random_species()
+          rarity  = roll_rarity(probs)
+          create_instance(species, rarity, trainer["username"])
+        end)
+
+        remaining_packs = List.delete(packs, found_pack)
+
+        updated_trainer =
+          trainer
+          |> Map.put("packs", remaining_packs)
+          |> Map.put("inventory", trainer["inventory"] ++ new_pokemon)
+
+        {:ok, updated_trainer, new_pokemon}
+    end
+  end
+
 end
