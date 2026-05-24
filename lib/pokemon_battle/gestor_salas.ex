43250967d0d57ae751228ae57b42ef4c3780fe37
johanspinedam-spec <johans.pinedam@uqvirtual.edu.co>
2026-05-24 01:00:10 -0500

    G. salas - create, join, start, list, get_room

--- lib/pokemon_battle/gestor_salas.ex
+++ lib/pokemon_battle/gestor_salas.ex
@@ -5,6 +5,14 @@ defmodule PokemonBattle.GestorSalas do
 
   def init(_), do: {:ok, %{rooms: %{}, counter: 0}}
 
+  def create_room(turn_time \\ 20), do: GenServer.call(__MODULE__, {:create_room, turn_time})
+  def join_room(room_id, trainer, team, pid \\ self()), do: GenServer.call(__MODULE__, {:join, room_id, trainer, team, pid})
+  def start_battle(room_id, username, requester_pid \\ nil) do
+    GenServer.call(__MODULE__, {:start, room_id, username, requester_pid})
+  end
+  def list_rooms, do: GenServer.call(__MODULE__, :list)
+  def get_room(room_id), do: GenServer.call(__MODULE__, {:get, room_id})
+
   
 
 end
