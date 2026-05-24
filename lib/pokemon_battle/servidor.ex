604915067df54648ece3b907f6f27d124c475de0
Juanfe-engineer <juanf.ibarral@uqvirtual.edu.co>
2026-05-24 01:13:15 -0500

    servidor - struct

---
+++ lib/pokemon_battle/servidor.ex
@@ -0,0 +1,14 @@
+defmodule PokemonBattle.Servidor do
+  alias PokemonBattle.GestorEntrenadores
+  alias PokemonBattle.SistemaSobres
+  alias PokemonBattle.GestorSalas
+  alias PokemonBattle.Intercambio
+  alias PokemonBattle.Cluster
+  alias PokemonBattle.Persistencia
+  alias PokemonBattle.Evolution
+
+  defstruct trainer: nil, current_team: nil, current_room: nil, trade_room: nil
+
+  
+
+end
